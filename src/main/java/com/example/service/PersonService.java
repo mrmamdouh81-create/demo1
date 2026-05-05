25a33b0e7669447758a608b8c5fba3376caf959b
Ahmed Mamdouh <mr.mamdouh81@gmail.com>
2026-05-05 11:12:29 -0700

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("5");
+		p.setAge(40);
+		p.setFirstName("docker");
+		p.setLastName("Mamdouh");
+		persons.add(p);ر
 		
 	}
 	public Person getPerson(String id) {
